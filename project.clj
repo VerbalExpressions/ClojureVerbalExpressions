213de0271cce3711c10504fc80388a2a83c6d4ba
Morten Linderud <mcfoxax@gmail.com>
2013-08-07 20:04:53 +0200

    removed snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojureverbalexpressions "0.1.0-SNAPSHOT"
+(defproject clojureverbalexpressions "0.1.1"
   :description "Port of the Verbal Expressions lib for Clojure"
   :url "http://github.com/Foxboron/clojureverbalexpressions"
   :license {:name "Eclipse Public License"
