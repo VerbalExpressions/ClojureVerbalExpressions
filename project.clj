1db4fadf2449da63066c2f554e79aeda003b2829
Morten Linderud <mcfoxax@gmail.com>
2013-08-07 20:01:41 +0200

    Lets make clojars happy

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject clojureverbalexpressions "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Port of the Verbal Expressions lib for Clojure"
+  :url "http://github.com/Foxboron/clojureverbalexpressions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles  {:dev  {:dependencies  [[midje "1.5.1"]]}})
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]]
+  :profiles  {:dev  {:dependencies  [[midje "1.5.1"]]}}
+  :scm {:name "git"
+        :url "https://github.com/Foxboron/clojureverbalexpressions"})
