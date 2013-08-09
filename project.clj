ea2e4b3766530180a8a5ab80f7d1a6418e58148a
Morten Linderud <mcfoxax@gmail.com>
2013-08-09 11:39:36 +0200

    Bumped version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojureverbalexpressions "0.2.0"
+(defproject clojureverbalexpressions "0.2.1"
   :description "Port of the Verbal Expressions lib for Clojure"
   :url "http://github.com/Foxboron/clojureverbalexpressions"
   :license {:name "Eclipse Public License"
